74e7d05fb0fdf948c17d8195c0955d1a05c60727
Aswnsyrf <aswnsyrf@gmail.com>
2024-03-22 04:34:03 +0800

    route

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\ProvinceController;
+use App\Http\Controllers\Api\CityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\Api\ProvinceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +27,13 @@ Route::prefix('/province')->group(function () {
     Route::put('/{id}', [ProvinceController::class, 'update']);
     Route::patch('/{id}', [ProvinceController::class, 'patch']);
     Route::delete('/{id}', [ProvinceController::class, 'delete']);
+});
+
+Route::prefix('/city')->group(function () {
+    Route::get('', [CityController::class, 'index']);
+    Route::post('', [CityController::class, 'create']);
+    Route::get('/{id}', [CityController::class, 'detail']);
+    Route::put('/{id}', [CityController::class, 'update']);
+    Route::patch('/{id}', [CityController::class, 'patch']);
+    Route::delete('/{id}', [CityController::class, 'delete']);
 });
